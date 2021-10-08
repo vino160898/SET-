89f2d00dc056fd400cbd4a004c2fc9e0aa0e4d4b
E.vinothkumar <90100624+vino160898@users.noreply.github.com>
2021-10-08 13:18:52 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,9 @@
+#set{}
+s={}      #-->dictionary
+s=set()   #-->create set 
+s={10,20,30,40}
+print(s)
+print(type(s))
+names=['professor','rio','danver','professor']
+s=set(names) #--> convert to set for duplicates remove.
+print(s)
